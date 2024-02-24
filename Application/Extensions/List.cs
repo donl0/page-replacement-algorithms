1a8c0e706ab684e6f9ede2bdf43886c786b0c4a0
donl0 <donl_117n@mail.ru>
2024-02-25 01:23:56 +0300

    Add optimal algorithm

---
+++ Application/Extensions/List.cs
@@ -0,0 +1,20 @@
+﻿namespace Application.Extensions
+{
+    public static class List
+    {
+        public static List<T> FarestElementAfterIndex<T>(this List<T> list, int index, List<T> listFindIn, out T item)
+        {
+            var indexes = list.Select(element =>
+            {
+                int indexOfElement = listFindIn.IndexOf(element, index + 1);
+                return indexOfElement >= 0 ? indexOfElement : int.MaxValue;
+            })
+           .ToList();
+
+            int maxIndex = indexes.IndexOf(indexes.Max());
+            item = list[maxIndex];
+
+            return list;
+        }
+    }
+}
---
+++ Application/Replacer/OptimalReplacer.cs
@@ -0,0 +1,38 @@
+﻿using Application.Extensions;
+using Application.Interfaces;
+using Domain.Interfaces.Replacer;
+
+namespace Application.Replacer
+{
+    public class OptimalReplacer : PageReplacer
+    {
+        private readonly ILoggerReplacer _logger;
+
+        private int _index = -1;
+
+        public OptimalReplacer(List<int> virtualMemory, List<int> primaryMemory, ILoggerReplacer logger) : base(virtualMemory, primaryMemory)
+        {
+            _logger = logger;
+        }
+
+        public override void NotifyAdded(int page)
+        {
+        }
+
+        public override void NotifyCurrentPage(int page)
+        {
+            _index++;
+        }
+
+        public override void Replace(int newPage)
+        {
+             PrimaryMemory.FarestElementAfterIndex<int>(_index, RequestOrder, out int pageToRemove);
+
+            RemovePage(pageToRemove);
+            AddPage(newPage);
+
+            _logger.Log(pageToRemove, newPage);
+        }
+
+    }
+}
