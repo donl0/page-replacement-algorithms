793cdef10b9d050f7841c747a6b5550966755874
donl0 <donl_117n@mail.ru>
2024-02-24 22:23:53 +0300

    Add abstractions for page replacer

---
+++ Domain/Interfaces/Replacer/IPageReplacer.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Interfaces.Replacer
+{
+    public interface IPageReplacer
+    {
+        public void Replace(int page);
+        public void NotifyCurrentPage(int page);
+        public void NotifyAdded(int page);
+    }
+}
---
+++ Domain/Interfaces/Replacer/PageReplacer.cs
@@ -0,0 +1,27 @@
+﻿namespace Domain.Interfaces.Replacer
+{
+    public abstract class PageReplacer : IPageReplacer
+    {
+        private readonly List<int> _requestOrder;
+        private List<int> _primaryMemory = new List<int>();
+
+        protected PageReplacer(List<int> virtualMemory, List<int> primaryMemory)
+        {
+            _requestOrder = virtualMemory;
+            _primaryMemory = primaryMemory;
+        }
+
+        protected void AddPage(int page) { 
+            _primaryMemory.Add(page);
+        }
+        protected void RemovePage(int page) { 
+            _primaryMemory.Remove(page);
+        }
+
+        public abstract void NotifyCurrentPage(int page);
+
+        public abstract void Replace(int page);
+
+        public abstract void NotifyAdded(int page);
+    }
+}
