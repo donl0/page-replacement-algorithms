c0d150b8347acb2e1d79fd2cb8c291d93f1b8a58
donl0 <donl_117n@mail.ru>
2024-02-24 22:33:04 +0300

    Add page adresser

---
+++ Application/Adresser/PageAdresser.cs
@@ -0,0 +1,39 @@
+﻿using Application.Interfaces;
+using Domain.Interfaces.Adresser;
+using Domain.Interfaces.Replacer;
+
+namespace Application.Adresser
+{
+    public class PageAdresser : BasePageAdresser
+    {
+        private readonly ILoggerAdresser _loggerAdresser;
+
+        public PageAdresser(IPageReplacer replacer, List<int> adressOrder, List<int> primaryMemory, int mainMemoryCapacity, ILoggerAdresser loggerAdresser) : base(replacer, adressOrder, primaryMemory, mainMemoryCapacity)
+        {
+            _loggerAdresser = loggerAdresser;
+        }
+
+        public override void Adressing()
+        {
+            foreach (var page in AdressOrder)
+            {
+                _loggerAdresser.Log(PrimaryMemory);
+
+                NotifyCurrentPage(page);
+
+                if (IsContainPage(page) == true)
+                    continue;
+
+                if (IsMemoryLessCapacity())
+                {
+                    Add(page);
+                    NotifyAdded(page);
+                }
+                else
+                {
+                    Replace(page);
+                }
+            }
+        }
+    }
+}
